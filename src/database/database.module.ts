d22c827ca98fc48e814beb0247d42702ffed23af
YilmarDev <yilmardev@gmail.com>
2024-07-01 15:29:06 -0500

    Clase 9: Conectandose a la BD con TypeOrm

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { Client } from 'pg';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import config from 'src/config';
 
 const API_KEY = '12345634';
@@ -8,6 +9,23 @@ const API_KEY_PROD = 'PROD1212121SA';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { host, port, database, user, password } = configService.postgres;
+
+        return {
+          type: 'postgres',
+          host,
+          port,
+          database,
+          username: user,
+          password,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -31,6 +49,6 @@ const API_KEY_PROD = 'PROD1212121SA';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'PG'],
+  exports: ['API_KEY', 'PG', TypeOrmModule],
 })
 export class DatabaseModule {}
